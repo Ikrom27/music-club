93e1bac74b149abd3d38995d2b8a2cdfe61ae775
Ikrom <74912914+Ikrom27@users.noreply.github.com>
2023-12-27 23:20:10 +0300

    new releases layout colors fix

--- app/src/main/java/com/ikrom/musicclub/ui/utils/ColorUtils.kt
+++ app/src/main/java/com/ikrom/musicclub/ui/utils/ColorUtils.kt
@@ -5,26 +5,19 @@ import android.graphics.Bitmap
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.core.graphics.ColorUtils
-import androidx.core.graphics.luminance
 import androidx.palette.graphics.Palette
 import com.google.android.material.color.utilities.Score
 
 @SuppressLint("RestrictedApi")
 fun Bitmap.extractThemeColor(): Color {
     val colorsToPopulation = Palette.from(this)
-        .maximumColorCount(8)
+        .maximumColorCount(24)
         .generate()
         .swatches
         .associate { it.rgb to it.population }
-
-    // Сортируем цвета по убыванию яркости (от темного к светлому)
-    val rankedColors = Score.score(colorsToPopulation).sortedByDescending { it.luminance }
-
-    // Выбираем первый (самый темный) цвет
-    val darkColor = Color(rankedColors.first())
-
-    // Понижаем яркость цвета вдвое
-    val finalColor = ColorUtils.blendARGB(Color.Black.toArgb(), darkColor.toArgb(), 0.3f)
-
+    val rankedColors = Score.score(colorsToPopulation)
+    val finalColor = ColorUtils.blendARGB(
+        Color.Black.toArgb(),
+        Color(rankedColors.first()).toArgb(), 0.3f)
     return Color(finalColor)
 }
